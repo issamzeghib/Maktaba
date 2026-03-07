842d4ae61b2ea19f3720deb01726059cc4dc3079
issamzeghib <issammimi308@gmail.com>
2026-03-07 23:40:23 +0100

    finish t2

--- app/src/main/java/com/ElOuedUniv/maktaba/data/repository/CategoryRepositoryImpl.kt
+++ app/src/main/java/com/ElOuedUniv/maktaba/data/repository/CategoryRepositoryImpl.kt
@@ -4,13 +4,40 @@ import com.ElOuedUniv.maktaba.data.model.Category
 
 class CategoryRepositoryImpl : CategoryRepository {
 
-    private val categoriesList = listOf<Category>()
-    
+    private val categoriesList = listOf(
+        Category(
+            id = "1",
+            name = "Programming",
+            description = "Books about software development and coding"
+        ),
+        Category(
+            id = "2",
+            name = "Algorithms",
+            description = "Books about algorithms and data structures"
+        ),
+        Category(
+            id = "3",
+            name = "Databases",
+            description = "Books about database design and management"
+        ),
+        Category(
+            id = "4",
+            name = "analysis",
+            description = "Books about analysis design and management"
+        ),
+        Category(
+            id = "5",
+            name = "Software engineering",
+            description = "Books about Software engineering design and management"
+        ),
+        // Add 2 more categories here
+    )
+
     override fun getAllCategories(): List<Category> {
-        TODO("Not yet implemented")
+        return categoriesList
     }
 
     override fun getCategoryById(id: String): Category? {
-        TODO("Not yet implemented")
+        return categoriesList.find { it.id == id }
     }
 }
